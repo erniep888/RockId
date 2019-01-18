f3722d37d16ce96f6f9b95b9280857e8eaa5500e
Ernie Paschall <epaschall@gmail.com>
2019-01-17 21:36:12 -0500

    Implemented mocked DbSet that supports LINQ and Async.

--- Source/Api/Domain/Repositories/IRockRepository.cs
+++ Source/Api/Domain/Repositories/IRockRepository.cs
@@ -1,12 +1,17 @@
 ﻿using RockId.Domain.Models;
 using System;
+using System.Threading.Tasks;
 
 namespace RockId.Domain.Repositories
 {
-    public interface IRockRepository
+    public interface IRockRepository : IDisposable
     {
         Rock FindByName(string name);
-        Rock FindById(Guid id);
-        Rock Save(Rock rock);
+        Task<Rock> FindById(Guid id);
+        void InsertRock(Rock rock);
+        void DeleteRock(Rock rock);
+        void DeleteRock(Guid id);
+        void UpdateRock(Rock rock);
+        void Save(Rock rock);
     }
 }
---
+++ Source/Api/Domain/Repositories/Implementations/RockIdDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RockId.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockId.Domain.Repositories.Implementations
+{
+    public class RockIdDbContext : DbContext    
+    {
+        public RockIdDbContext()
+        {
+        }
+
+        public RockIdDbContext(DbContextOptions<RockIdDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Rock> Rocks { get; set; }
+    }
+}
