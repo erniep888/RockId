abcebeed77d81b5f21f5f9777d66237b47e8c946
Ernie Paschall <epaschall@gmail.com>
2019-02-11 11:30:36 -0500

    #84 - Initial implementation of the Ocelot Backend For Frontend - SPA Gateway.  It currenlty routes to port 80.

---
+++ Source/ApiGateway/BackendForFrontend_Spa/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace RockId.BackendForFrontend_Spa
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices( service => service.AddSingleton(builder))
+                 .ConfigureAppConfiguration( appConf => appConf.AddJsonFile(Path.Combine("Configuration", "configuration.json")))
+                 .UseStartup<Startup>()
+                 .UseSerilog((builderContext, config) => 
+                 {
+                     config
+                        .MinimumLevel.Information()
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console();
+                 });
+            IWebHost host = builder.Build();
+
+            return host;
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
