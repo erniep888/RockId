36373878b8ff03c38fb297ed0bdc2bb2d622ce15
Ernie Paschall <epaschall@gmail.com>
2019-02-14 09:18:10 -0500

    #77 - This implementation allows the docker-compose networking to work correctly.  However, I am switching everything to kubernetes with the next checkin.

--- Source/Service/Qarp/Qarp.GlassyRock/Startup.cs
+++ Source/Service/Qarp/Qarp.GlassyRock/Startup.cs
@@ -41,7 +41,7 @@ namespace Qarp.GlassyRock
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
